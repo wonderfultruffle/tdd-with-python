c68dd18f0a9a909e5cd73b637dc047af9aa950d6
jh <jh2two@gmail.com>
2023-12-24 20:22:49 +0900

    Test: FT Refoactor 수정

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTesT(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(value=g"id_list_table")
+        rows = table.find_elements("tag name", "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_todo_list(self):
         # 사용자는 To-Do 웹 사이트에 방문한다.
         self.browser.get("http://localhost:8000")
@@ -28,22 +33,26 @@ class NewVisitorTesT(unittest.TestCase):
         
         # "공작 깃털 사기" 라는 텍스트 상자에 추가한다.
         input_box.send_keys("공작 깃털 사기")
-        
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에 '1: 공작 깃털 사기' 아이템이 추가된다.
         input_box.send_keys(Keys.ENTER)
         self.browser.implicitly_wait(1)
-        
-        table = self.browser.find_element(value="id_list_table")
-        rows = table.find_elements("tag name", "tr")
-        self.assertTrue(any(row.text == "1: 공작 깃털 사기" for row in rows), "신규 작업이 테이블에 추가되지 않음.")
+
+        self.check_for_row_in_list_table("1: 공작 깃털 사기")
         
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
+        input_box = self.browser.find_element(value="id_new_item")
         # 이번엔 "공작 깃털을 이용해 그물 만들기"라고 텍스트 상자에 입력하고 엔터키를 친다.
-        self.fail("Finish the test!")
+        input_box.send_keys("공작 깃털을 이용해 그물 만들기")
+        input_box.send_keys(Keys.ENTER)
+        self.browser.implicitly_wait(1)
 
         # 페이지는 다시 갱신되고 두 개의 작업 목록이 존재한다.
+        self.check_for_row_in_list_table("1: 공작 깃털 사기")
+        self.check_for_row_in_list_table("2: 공작 깃털을 이용해 그물 만들기")
+
         # 사이트는 사용자에게 특정 URL을 생성해준다.
         # 이때 URL에 대한 설명도 함께 제공된다.
+        self.fail("Finish the test!")
 
         # 해당 URL에 접속하면 사용자가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
 
