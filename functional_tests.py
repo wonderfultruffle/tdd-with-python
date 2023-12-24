b82d3663103d87b5f5cf1cda0cb211626cdefc26
jh <jh2two@gmail.com>
2023-12-24 22:17:58 +0900

    Fix: 오타수정

--- functional_tests.py
+++ functional_tests.py
@@ -14,7 +14,7 @@ class NewVisitorTesT(unittest.TestCase):
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(value=g"id_list_table")
+        table = self.browser.find_element(value="id_list_table")
         rows = table.find_elements("tag name", "tr")
         self.assertIn(row_text, [row.text for row in rows])
 
