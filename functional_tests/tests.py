080438b410849d927e79c807a1de3540eeeb266d
kjh <jh2two@gmail.com>
2023-12-28 00:12:08 +0900

    Chore: 오타 수정

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 from django.test import LiveServerTestCase
 
 import time
+
+MAX_WAIT = 5
 
 class NewVisitorTesT(LiveServerTestCase):
 
@@ -14,11 +17,20 @@ class NewVisitorTesT(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(value="id_list_table")
-        rows = table.find_elements("tag name", "tr")
-        self.assertIn(row_text, [row.text for row in rows])
-
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        
+        while True:
+            try:
+                table = self.browser.find_element(value="id_list_table")
+                rows = table.find_elements("tag name", "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException):
+                if time.time() - start_time > MAX_WAIT:
+                    raise
+                time.sleep(0.5)
+            
     def test_can_start_a_todo_list(self):
         # 사용자는 To-Do 웹 사이트에 방문한다.
         self.browser.get(self.live_server_url)
@@ -36,20 +48,17 @@ class NewVisitorTesT(LiveServerTestCase):
         input_box.send_keys("공작 깃털 사기")
         # 엔터키를 치면 페이지가 갱신되고 작업 목록에 '1: 공작 깃털 사기' 아이템이 추가된다.
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table("1: 공작 깃털 사기")
+        self.wait_for_row_in_list_table("1: 공작 깃털 사기")
         
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         input_box = self.browser.find_element(value="id_new_item")
         # 이번엔 "공작 깃털을 이용해 그물 만들기"라고 텍스트 상자에 입력하고 엔터키를 친다.
         input_box.send_keys("공작 깃털을 이용해 그물 만들기")
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
-        # self.browser.implicitly_wait(5)
 
         # 페이지는 다시 갱신되고 두 개의 작업 목록이 존재한다.
-        self.check_for_row_in_list_table("1: 공작 깃털 사기")
-        self.check_for_row_in_list_table("2: 공작 깃털을 이용해 그물 만들기")
+        self.wait_for_row_in_list_table("1: 공작 깃털 사기")
+        self.wait_for_row_in_list_table("2: 공작 깃털을 이용해 그물 만들기")
 
         # 사이트는 사용자에게 특정 URL을 생성해준다.
         # 이때 URL에 대한 설명도 함께 제공된다.
