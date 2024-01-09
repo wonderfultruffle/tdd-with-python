fa0a791af99851240773cb0fef18f7f145454599
jh <jh2two@gmail.com>
2024-01-09 23:21:48 +0900

    Refactor: 부트스트랩 사용을 위한 template 상속 도입, 부트스트랩 적용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,12 +3,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 import time
 
 MAX_WAIT = 5
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Chrome()
