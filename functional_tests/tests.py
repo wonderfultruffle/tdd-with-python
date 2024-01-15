fab50fa8846c11724ef5352514c4ed1387b10e1d
jh <jh2two@gmail.com>
2024-01-15 23:59:15 +0900

    Test: Staging Server Test를 위한 FT 실행자 해킹

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -14,6 +16,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
+
+        staging_server = os.environ.get("STAGING_SERVER")
+        if staging_server:
+            self.live_server_url = f"http://{staging_server}"
 
     def tearDown(self):
         self.browser.quit()
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path("", views.home_page, name="home"),
     path("lists/", include("lists.urls")),
 ]
